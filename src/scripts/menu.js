569e3eb64e19029b0353110f7cb9252e3e099b33
mad4thrash <fabio.plumeri@oisluxurygroup.com>
2024-01-22 16:36:14 +0100

    animation on menu and Hamburger

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,6 +1,13 @@
 
+const lineOne = document.querySelector('.line-one');
+const lineTwo = document.querySelector('.line-two');
+const lineThree = document.querySelector('.line-three');
+
 document.addEventListener('astro:page-load', () => {
   document.querySelector('.hamburger').addEventListener('click', () => {
     document.querySelector('.nav-links').classList.toggle('expanded');
+    lineOne.classList.toggle('line-one-expanded');
+    lineTwo.classList.toggle('line-two-expanded');
+    lineThree.classList.toggle('line-three-expanded');
   });
 });
